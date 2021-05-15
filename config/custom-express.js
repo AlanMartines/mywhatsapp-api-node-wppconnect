7c80ec22c0c23b2cd3cc15b44171aa711a643860
Alan Martines <alancpmartines@hotmail.com>
2021-05-15 15:31:38 -0400

    Inicial Update

--- config/custom-express.js
+++ config/custom-express.js
@@ -11,7 +11,14 @@ const sistem = require("../controllers/sistem.controller");
 //
 module.exports = () => {
   const http = require('http').createServer({}, app);
+  const io = require('socket.io')(http);
   app.use(cors());
+  //
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   //
   // Express Parser
   app.use(express.json({
@@ -24,6 +31,25 @@ module.exports = () => {
     extended: true
   }));
   //
+  const sockets = {};
+  //socket
+  //
+  //cria um callback para quando alguém conectar
+  //socket => instancia da conexão do usuário
+  io.on('connection', (socket) => {
+    //adiciona todas os id's do socket na variavel sockets
+    sockets[socket.id] = socket;
+    console.log('socketid ' + socket.id);
+  });
+  //
+  //socket
+  io.on('connection', (socket) => {
+    socket.on('disconnect', function() {
+      console.log('-------');
+      console.log('Fechou conexão');
+      console.log('socketid ' + socket.id);
+    });
+  });
   //
   // Rotas
   app.use("/sistema", sistem);
