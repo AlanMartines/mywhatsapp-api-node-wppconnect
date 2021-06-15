9f2fa0e9e74282c5db4cf883f97ced6db6ef234e
Alan Martines <alancpmartines@hotmail.com>
2021-06-15 08:11:13 -0400

    Inicial Update

--- README.md
+++ README.md
@@ -190,7 +190,8 @@ mkdir c:\tokens
 {
   "_comment_host": "Configuração do host",
   "host": "0.0.0.0",
-  "port": "9000",
+  "port": "9001",
+  "view_qrcode_terminal": true,
   "_comment_jwt": "Configuração do jwt",
   "jwt_secret": "09f26e402586e2faa8da4c98a35f1b20d6b033c60",
   "_comment_token": "Pasta de tokens",
--- server.js
+++ server.js
@@ -8,11 +8,18 @@ fs.access("./config/server.config.json", fs.constants.F_OK, (err) => {
         "_comment_host": "Configuração do host",
         "host": "0.0.0.0",
         "port": "9001",
+        "view_qrcode_terminal": true,
         "_comment_jwt": "Configuração do jwt",
         "jwt_secret": "09f26e402586e2faa8da4c98a35f1b20d6b033c60",
         "_comment_token": "Pasta de tokens",
         "tokenspatch_linux": "/usr/local/tokens",
-        "tokenspatch_win": "c:/tokens"
+        "tokenspatch_win": "c:/tokens",
+        "_comment_mysql": "Conexão com bando de dados",
+        "validate_mysql": true,
+        "host_mysql": "localhost",
+        "user_mysql": "user",
+        "password_mysql": "password",
+        "database_mysql": "database"
       };
       console.log("- Modelo do arquivo de configuração:\n", modelo);
       process.exit(1);
