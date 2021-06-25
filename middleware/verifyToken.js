2ee5d71a680d205e555e2e5e24d1d4c28e518e06
Alan Martines <alancpmartines@hotmail.com>
2021-06-24 23:42:38 -0400

    Inicial Update

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -6,7 +6,7 @@ var todayDate = new Date().toISOString().slice(0, 10);
 exports.verify = async (req, res, next) => {
   try {
     if (serverConfig.validate_mysql == true) {
-      if (!req.body.SessionName) {
+      if (!req.headers['authorization'] && !req.body.AuthorizationToken && !req.body.SessionName) {
         res.status(422).json({
           "Status": {
             "result": "info",
