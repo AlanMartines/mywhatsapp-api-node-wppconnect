42e4d787a764d4e631123d7894a9ba57c52b2ecb
Alan Martines <alancpmartines@hotmail.com>
2021-08-15 09:57:00 -0400

    Inicial Update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,32 +1,33 @@
 const mysql = require("mysql2");
-const serverConfig = require("../config/server.config.json");
-//
-// connect to the db
-const dbConnectionInfo = {
-  host: serverConfig.host_mysql, // O host do banco. Ex: localhost
-  user: serverConfig.user_mysql, // Um usuário do banco. Ex: user 
-  password: serverConfig.password_mysql, // A senha do usuário. Ex: user123
-  database: serverConfig.database_mysql, // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-};
-//
-//For mysql single connection
-/* var dbconnection = mysql.createConnection(
-        dbConnectionInfo
-); 
-//
- dbconnection.connect(function (err) {
-    if (!err) {
-        console.log("Database is connected ...");
-    } else {
-        console.log("Error connecting database ...");
-    }
-}); 
-//
-*/
 if (serverConfig.validate_mysql === true) {
+  const serverConfig = require("../config/server.config.json");
+  //
+  // connect to the db
+  const dbConnectionInfo = {
+    host: serverConfig.host_mysql, // O host do banco. Ex: localhost
+    user: serverConfig.user_mysql, // Um usuário do banco. Ex: user 
+    password: serverConfig.password_mysql, // A senha do usuário. Ex: user123
+    database: serverConfig.database_mysql, // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  };
+  //
+  //For mysql single connection
+  /* var dbconnection = mysql.createConnection(
+          dbConnectionInfo
+  ); 
+  //
+   dbconnection.connect(function (err) {
+      if (!err) {
+          console.log("Database is connected ...");
+      } else {
+          console.log("Error connecting database ...");
+      }
+  }); 
+  //
+  */
+
   //Create mysql connection pool
   var dbconnection = mysql.createPool(
     dbConnectionInfo
