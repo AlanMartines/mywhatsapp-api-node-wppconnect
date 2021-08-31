7578fff1f7a80582a32e13076a4dd883d6739e99
Alan Martines <alancpmartines@hotmail.com>
2021-08-30 22:12:36 -0400

    Inicial Update

--- sessions.js
+++ sessions.js
@@ -17,7 +17,7 @@ const con = require("./config/dbConnection");
 //
 // ------------------------------------------------------------------------------------------------------- //
 //
-async function DataHora(dateFormat) {
+async function DataHora() {
   //
   let date_ob = new Date();
 
@@ -103,11 +103,11 @@ async function osplatform() {
 //
 async function updateStateDb(state, status, session_venom) {
   //
+  const sql = "UPDATE tokens SET state=?, status=? WHERE token=?";
+  const values = [state, status, session_venom];
+  //
   if (serverConfig.validate_mysql == true) {
-    //
     const conn = require('./config/dbConnection').promise();
-    const sql = "UPDATE tokens SET state=?, status=? WHERE token=?";
-    const values = [state, status, session_venom];
     const resUpdate = await conn.execute(sql, values);
     if (resUpdate) {
       console.log('- Status atualizado');
