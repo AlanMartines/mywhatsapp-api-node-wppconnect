d881301782880abe6dc4c67bcd8e4969e9e79900
Alan Martines <alancpmartines@hotmail.com>
2021-09-19 17:33:05 -0400

    Inicial Update

--- sessions.js
+++ sessions.js
@@ -748,7 +748,7 @@ module.exports = class Sessions {
       updatesLog: true, // Logs info updates automatically in terminal
       autoClose: false, // Automatically closes the WPPConnect only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
       tokenStore: 'file', // Define how work with tokens, that can be a custom interface
-      folderNameToken: session.tokenPatch, //folder name when saving tokens
+      folderNameToken: session.tokenPatch //folder name when saving tokens
       //createPathFileToken: true, //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
     });
     // Levels: 'error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly'
