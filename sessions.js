767bf3854ef75b02af90a97791aba563a9b4d7a1
Alan Martines <alancpmartines@hotmail.com>
2021-10-08 16:36:06 -0400

    Inicial Update

--- sessions.js
+++ sessions.js
@@ -116,7 +116,7 @@ async function updateStateDb(state, status, SessionName) {
   const sql = "UPDATE tokens SET state=?, status=?, lastactivit=? WHERE token=?";
   const values = [state, status, date_now, SessionName];
   //
-  if (config.VALIDATE_MYSQL == 'true') {
+  if (Boolean(config.VALIDATE_MYSQL) == true) {
     console.log('- Atualizando status');
     const conn = require('./config/dbConnection').promise();
     const resUpdate = await conn.execute(sql, values);
@@ -691,7 +691,7 @@ module.exports = class Sessions {
       devtools: false, // Open devtools by default
       useChrome: true, // If false will use Chromium instance
       debug: false, // Opens a debug session
-      logQR: config.VIEW_QRCODE_TERMINAL, // Logs QR automatically in terminal
+      logQR: Boolean(config.VIEW_QRCODE_TERMINAL), // Logs QR automatically in terminal
       browserWS: '', // If u want to use browserWSEndpoint
       browserArgs: [
         '--log-level=3',
