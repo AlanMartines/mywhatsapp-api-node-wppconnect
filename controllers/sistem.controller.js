d9fafe5acd8a7e410e4dd2087c9922f94f479eb6
Alan Martines <alancpmartines@hotmail.com>
2021-10-29 00:07:45 -0400

    Inicial Update

--- controllers/sistem.controller.js
+++ controllers/sistem.controller.js
@@ -8,6 +8,24 @@ const fs = require('fs-extra');
 const path = require('path');
 const express = require("express");
 const multer = require('multer');
+// Configuração de armazenamento
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function(req, file, cb) {
+    // Extração da extensão do arquivo original:
+    const extensaoArquivo = file.originalname.split('.')[1];
+
+    // Cria um código randômico que será o nome do arquivo
+    const novoNomeArquivo = require('crypto')
+      .randomBytes(64)
+      .toString('hex');
+
+    // Indica o novo nome do arquivo:
+    cb(null, `${novoNomeArquivo}.${extensaoArquivo}`)
+  }
+});
 const upload = multer({})
 const router = express.Router();
 const Sessions = require("../sessions.js");
