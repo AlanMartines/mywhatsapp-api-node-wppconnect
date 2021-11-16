4b92afb3b74bb045c85bc98c1b476660f059e611
Alan Martines <alancpmartines@hotmail.com>
2021-11-16 10:41:23 -0400

    Inicial Update

--- sessions.js
+++ sessions.js
@@ -1203,7 +1203,7 @@ module.exports = class Sessions {
         };
         //
       }).catch((erro) => {
-        //console.error("Error when sending: ", erro); //return object error
+        console.error("Error when sending: ", erro); //return object error
         //return { result: 'error', state: session.state, message: "Erro ao enviar menssagem" };
         //return (erro);
         //
