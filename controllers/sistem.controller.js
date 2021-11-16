fd62c4660b3f68e64ac9b7d10fd02bf5520d57e1
Alan Martines <alancpmartines@hotmail.com>
2021-11-16 13:14:59 -0400

    Inicial Update

--- controllers/sistem.controller.js
+++ controllers/sistem.controller.js
@@ -98,7 +98,7 @@ const convertBytes = function(bytes) {
 //
 router.post("/Start", upload.none(''), verifyToken.verify, async (req, res, next) => {
   //
-  var sessionStatus = await Sessions.ApiStatus(req.body.SessionName.trim());
+  var sessionStatus = await Sessions.ApiStatus(req.body.SessionName.replace(/\s+/g, ''));
   switch (sessionStatus.status) {
     case 'inChat':
     case 'qrReadSuccess':
@@ -123,9 +123,9 @@ router.post("/Start", upload.none(''), verifyToken.verify, async (req, res, next
     case 'DISCONNECTED':
     case 'qrRead':
       //
-      var getStart = await Sessions.Start(req.body.SessionName.trim(), req.body.AuthorizationToken.trim());
-      var session = Sessions.getSession(req.body.SessionName.trim());
-      console.log("- AuthorizationToken:", req.body.AuthorizationToken.trim());
+      var getStart = await Sessions.Start(req.body.SessionName.replace(/\s+/g, ''), req.body.AuthorizationToken.replace(/\s+/g, ''));
+      var session = Sessions.getSession(req.body.SessionName.replace(/\s+/g, ''));
+      console.log("- AuthorizationToken:", req.body.AuthorizationToken.replace(/\s+/g, ''));
       session.state = 'STARTING';
       session.status = 'notLogged';
       var Start = {
