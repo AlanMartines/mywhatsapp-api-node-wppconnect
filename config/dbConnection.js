fec55a11372570c165f5f43e4918181842574da8
Alan Martines <alancpmartines@hotmail.com>
2021-11-20 19:10:33 -0400

    Inicial Update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,35 +1,39 @@
 const mysql = require("mysql2");
-//require('dotenv/config');
-/*
-require("dotenv").config({
-  path: "./.env"
-});
-*/
 const config = require('../config.global');
 //
 // connect to the db
-const dbConnectionInfo = {
+const dbConnection = {
+  host: config.HOST_MYSQL, // O host do banco. Ex: localhost
+  port: config.PORT_MYSQL, // Port do banco. Ex: 3306
+  user: config.USER_MYSQL, // Um usuário do banco. Ex: user 
+  password: config.PASSWORD_MYSQL, // A senha do usuário. Ex: user123
+  database: config.DATABASE_MYSQL // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
+};
+//
+// connect to the db
+const dbConnectionPool = {
   host: config.HOST_MYSQL, // O host do banco. Ex: localhost
   port: config.PORT_MYSQL, // Port do banco. Ex: 3306
   user: config.USER_MYSQL, // Um usuário do banco. Ex: user 
   password: config.PASSWORD_MYSQL, // A senha do usuário. Ex: user123
   database: config.DATABASE_MYSQL, // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  waitForConnections: true, // Determina a ação do pool quando nenhuma conexão está disponível e o limite foi atingido.
+  connectionLimit: 10, // O número máximo de conexões a serem criadas de uma vez. (Padrão: 10)
+  queueLimit: 0, // O número máximo de solicitações de conexão que o pool irá enfileirar antes de retornar um erro
+  getConnection: 0 //Se definido como 0, não há limite para o número de solicitações de conexão na fila. (Padrão: 0)
 };
 //
 //For mysql single connection
-/* 
 //Create mysql connection pool
 var dbconnection = mysql.createPool(
-  dbConnectionInfo
+  dbConnectionPool
 );
-*/
+/*
 //Create mysql connection
 var dbconnection = mysql.createConnection(
-  dbConnectionInfo
+  dbConnection
 );
+*/
 //
 //
 dbconnection.connect(function(err) {
@@ -52,7 +56,7 @@ dbconnection.on('connection', function(connection) {
   });
   //
   dbconnection.end(function(err) {
-    console.log('- All connections in the pool have ended');
+    console.log('- All connections the have ended');
   });
 });
 //
