5b41d71ca4285eecc2d9a539f01d7f2732573092
Alan Martines <alancpmartines@hotmail.com>
2021-11-21 14:03:22 -0400

    Inicial Update

--- middleware/verifyBody.js
+++ middleware/verifyBody.js
@@ -96,11 +96,23 @@ exports.Usage = async (req, res, next) => {
 //
 exports.sendVoice = async (req, res, next) => {
   //
+  const FILE_SIZE = 160 * 1024;
+  const SUPPORTED_FORMATS = [
+    "image/jpg",
+    "image/jpeg",
+    "image/gif",
+    "image/png"
+  ];
+  //
   let validationSchema = yup.object().shape({
     AuthorizationToken: yup.string(),
     SessionName: yup.string().required(),
     phonefull: yup.string().required(),
-    file: yup.mixed().required()
+    file: yup
+      .mixed()
+      .required("A file is required")
+      .test("fileSize", "File too large", value => value && value.size <= FILE_SIZE)
+      .test("fileFormat", "Unsupported Format", value => value && SUPPORTED_FORMATS.includes(value.type))
   });
   //
   await validateBody(validationSchema, req, res, next);
