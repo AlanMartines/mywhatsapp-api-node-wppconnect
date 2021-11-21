8eec6c52ceaf46f8da81e2179ecb66140e7013ff
Alan Martines <alancpmartines@hotmail.com>
2021-11-21 10:56:44 -0400

    Inicial Update

--- middleware/verifyBody.js
+++ middleware/verifyBody.js
@@ -85,7 +85,7 @@ exports.sendVoice = async (req, res, next) => {
     AuthorizationToken: yup.string(),
     SessionName: yup.string().required(),
     phonefull: yup.string().required(),
-    audio_data: yup.mixed().notType()
+    audio_data: yup.mixed().required()
   });
   //
   await validateBody(schema, req, res, next);
