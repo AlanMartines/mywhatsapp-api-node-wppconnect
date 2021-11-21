f0000072c61319adb1ce99614bf8d4490bbe20a5
Alan Martines <alancpmartines@hotmail.com>
2021-11-21 12:16:45 -0400

    Inicial Update

--- middleware/verifyBody.js
+++ middleware/verifyBody.js
@@ -85,7 +85,11 @@ exports.sendVoice = async (req, res, next) => {
     AuthorizationToken: yup.string(),
     SessionName: yup.string().required(),
     phonefull: yup.string().required(),
-    file: yup.mixed().required('File is required')
+    file: yup.mixed().required('A file is required')
+      .test('fileFormat', 'PDF only', (value) => {
+        console.log(value);
+        return value && ['application/pdf'].includes(value.type);
+      })
   });
   //
   await validateBody(schema, req, res, next);
