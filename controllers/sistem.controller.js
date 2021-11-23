ca74b3b65fcb0cd8c1d08406397f150f5cf472f6
Alan Martines <alancpmartines@hotmail.com>
2021-11-23 00:35:21 -0400

    Inicial Update

--- controllers/sistem.controller.js
+++ controllers/sistem.controller.js
@@ -220,7 +220,7 @@ router.post("/Logout", upload.none(''), verifyBody.Started, verifyToken.verify, 
 // ------------------------------------------------------------------------------------------------//
 //
 // Gera o QR-Code
-router.post("/QRCode", upload.none(''), verifyToken.Started, verifyToken.verify, async (req, res, next) => {
+router.post("/QRCode", upload.none(''), verifyToken.verify, async (req, res, next) => {
   console.log("- getQRCode");
   var sessionStatus = await Sessions.ApiStatus(removeWithspace(req.body.SessionName));
   var session = Sessions.getSession(removeWithspace(req.body.SessionName));
