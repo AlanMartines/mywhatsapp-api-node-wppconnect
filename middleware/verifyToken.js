e8589b8ed078a13369dee2dcd9e8e71ed2e00558
Alan Martines <alancpmartines@hotmail.com>
2021-12-07 11:05:17 -0400

    Inicial Update

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -47,7 +47,7 @@ exports.verify = async (req, res, next) => {
             });
           }
           //
-          const conn = require('../config/dbConnection');
+          const conn = require('../config/dbConnection').promise();
           const sql = "SELECT * FROM tokens WHERE token=? LIMIT 1";
           const values = [theTokenAuth];
           const [row] = await conn.promise().execute(sql, values);
