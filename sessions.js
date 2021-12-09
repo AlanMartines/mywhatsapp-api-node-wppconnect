cf582ea2e424a07983a5093188b924918f558a32
Alan Martines <alancpmartines@hotmail.com>
2021-12-09 09:42:38 -0400

    Inicial Update

--- sessions.js
+++ sessions.js
@@ -598,7 +598,6 @@ module.exports = class Sessions {
         '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
         //Outros
         '--disable-web-security',
-        '--disable-web-security',
         '--aggressive-cache-discard',
         '--disable-cache',
         '--disable-application-cache',
