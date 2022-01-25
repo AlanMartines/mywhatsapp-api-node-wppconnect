2d985cb3782647fde9498b402061705417003b72
Alan Martines <alancpmartines@hotmail.com>
2022-01-24 21:53:22 -0400

    Update

--- middleware/validateJson.js
+++ middleware/validateJson.js
@@ -3,17 +3,9 @@ const Validator = require('jpv');
 //
 exports.verify = async (req, res, next) => {
 	//
-	const json = {
-		status : "OK",
-		data : {
-			url : "http://example.com"
-		}
-	}
-	//
 	try {
-		const value = Validator.validate(json) // true
 	//
-	console.log("Validator", value);
+	console.log("Validator", req);
 	//
 	} catch (error) {
 	//
