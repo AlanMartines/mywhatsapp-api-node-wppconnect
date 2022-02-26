814cda3baeb5f7d81c5d222230aa56d055605425
Alan Martines <alancpmartines@hotmail.com>
2022-02-26 17:42:52 -0400

    Update

--- controllers/sistem.controller.js
+++ controllers/sistem.controller.js
@@ -147,12 +147,10 @@ router.post("/Start", upload.none(''), verifyToken.verify, async (req, res, next
 			case 'qrRead':
 				//
 				var data = {
-					SessionName: removeWithspace(req.body.SessionName),
-					MultiDevice: req.body.MultiDevice,
-					whatsappVersion: req.body.whatsappVersion
+					"SessionName": removeWithspace(req.body.SessionName),
+					"MultiDevice": req.body.MultiDevice,
+					"whatsappVersion": req.body.whatsappVersion
 				};
-				//
-				var data = JSON.parse(req.body);
 				//
 				const confToken = await startAll.confToken(`${config.tokenPatch}`, `${data.SessionName}.auto.json`, null, true);
 				//
