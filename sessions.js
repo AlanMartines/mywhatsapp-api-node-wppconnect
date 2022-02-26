d2e026fd4709fa09916621ab0a71ccc124f74b42
Alan Martines <alancpmartines@hotmail.com>
2022-02-26 17:23:08 -0400

    Update

--- sessions.js
+++ sessions.js
@@ -649,7 +649,7 @@ module.exports = class Sessions {
           '--disable-dev-shm-usage',
           '--disable-gl-drawing-for-tests',
           '--incognito',
-          '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
+          //'--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
           //Outros
           '--disable-web-security',
           '--aggressive-cache-discard',
