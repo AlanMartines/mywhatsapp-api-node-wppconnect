fa28caddbbf14ec8f6625d44d138f9fa011ff17e
Alan Martines <alancpmartines@hotmail.com>
2022-02-26 12:25:14 -0400

    Update

--- config.global.js
+++ config.global.js
@@ -9,10 +9,10 @@ config.DEVICE_NAME = process.env.DEVICE_NAME || 'My Whatsapp';
 config.AUTO_CLOSE = process.env.AUTO_CLOSE || 60000;
 config.JWT_SECRET = process.env.JWT_SECRET || '09f26e402586e2faa8da4c98a35f1b20d6b033c60';
 config.VALIDATE_MYSQL = process.env.VALIDATE_MYSQL || 0;
+config.MYSQL_DIALECT = process.env.MYSQL_ENGINE || 'mysql';
 config.MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
 config.MYSQL_PORT = process.env.MYSQL_PORT || '3306';
 config.MYSQL_USER = process.env.MYSQL_USER || 'root';
-config.MYSQL_DIALECT = process.env.MYSQL_DIALECT || 'mysql';
 config.MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '';
 config.MYSQL_DATABASE = process.env.MYSQL_DATABASE || '';
 config.MYSQL_TIMEZONE = process.env.MYSQL_TIMEZONE || '-04:00';
@@ -22,3 +22,4 @@ config.FORCE_CONNECTION_USE_HERE = process.env.FORCE_CONNECTION_USE_HERE || 0;
 //
 config.tokenPatch = "/usr/local/tokens";
 //
+console.log(config);
