44b1faf423adc9c779743d5a614fae9c1a01aa7d
AlanMartines <alancpmartines@hotmail.com>
2022-03-15 14:32:47 -0400

    Update

--- controllers/sistem.controller.js
+++ controllers/sistem.controller.js
@@ -432,7 +432,7 @@ router.post("/getSession", upload.none(''), verifyToken.verify, async (req, res,
 		//
 		var getSession = await Sessions.getSession(removeWithspace(req.body.SessionName));
 		//
-		//console.log(result);
+		console.log(getSession);
 		res.setHeader('Content-Type', 'application/json');
 		res.status(200).json({
 			getSession
@@ -461,7 +461,7 @@ router.post("/getSessions", upload.none(''), verifyToken.verify, async (req, res
 		//
 		var getSessions = await Sessions.getAll();
 		//
-		//console.log(result);
+		console.log(getSessions);
 		res.setHeader('Content-Type', 'application/json');
 		res.status(200).json({
 			getSessions
