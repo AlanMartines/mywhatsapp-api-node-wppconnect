1c879bc526cbb677da2cb8d1475820d82a7427e8
Alan Martines <alancpmartines@hotmail.com>
2022-04-02 14:46:56 -0400

    Update

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ FORCE_CONNECTION_USE_HERE=0
 			app.set('json spaces', 2);
 			//app.use(express.static('./public'));
 			//app.use('./public', express.static('public'));
-			app.use(express.static(__dirname +'/public'));
+			app.use(express.static(__dirname + '/public'));
 
 			//
 			app.use((req, res, next) => {
