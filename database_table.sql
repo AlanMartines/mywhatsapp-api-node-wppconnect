a01481820121159def89652de4de8e4ae5a296d7
AlanMartines <alancpmartines@hotmail.com>
2022-04-04 15:32:44 -0400

    Update

--- database_table.sql
+++ database_table.sql
@@ -1,4 +1,3 @@
-
 -- Copiando estrutura do banco de dados para mywhatsapp-api
 CREATE DATABASE IF NOT EXISTS `mywhatsapp-api`;
 USE `mywhatsapp-api`;
@@ -7,12 +6,11 @@ USE `mywhatsapp-api`;
 CREATE TABLE IF NOT EXISTS `tokens` (
   `ID` int unsigned NOT NULL AUTO_INCREMENT,
   `token` char(255) NOT NULL,
-	`webhook` char(255) NULL,
+	`sessionToken` text DEFAULT NULL,
+	`webhook` char(255) DEFAULT NULL,
   `status` char(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'false',
   `state` char(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'false',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `lastactivit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb3;
-
--- Exportação de dados foi desmarcado.
