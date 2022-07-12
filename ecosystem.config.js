8b2063c2899ebc5c39af0a32853ec351f4d609c8
Alan Martines <alancpmartines@hotmail.com>
2022-07-11 22:21:41 -0400

    Initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,15 +10,7 @@ const VIEW_QRCODE_TERMINAL = process.env.VIEW_QRCODE_TERMINAL || 1;
 const DEVICE_NAME = process.env.DEVICE_NAME || 'My Whatsapp';
 const WA_VERSION = process.env.WA_VERSION || '';
 const AUTO_CLOSE = process.env.AUTO_CLOSE || 60000;
-const JWT_SECRET = process.env.JWT_SECRET || '09f26e402586e2faa8da4c98a35f1b20d6b033c60';
-const VALIDATE_MYSQL = process.env.VALIDATE_MYSQL || 0;
-const MYSQL_DIALECT = process.env.MYSQL_ENGINE || 'mysql';
-const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
-const MYSQL_PORT = process.env.MYSQL_PORT || '3306';
-const MYSQL_USER = process.env.MYSQL_USER || 'root';
-const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '';
-const MYSQL_DATABASE = process.env.MYSQL_DATABASE || '';
-const MYSQL_TIMEZONE = process.env.MYSQL_TIMEZONE || '-04:00';
+const SECRET_KEY = process.env.SECRET_KEY || '09f26e402586e2faa8da4c98a35f1b20d6b033c60';
 const BROWSER_WSENDPOINT = process.env.BROWSER_WSENDPOINT || undefined;
 const START_ALL_SESSIONS = process.env.START_ALL_SESSIONS || 0;
 const useHere = process.env.FORCE_CONNECTION_USE_HERE || 0;
@@ -44,18 +36,8 @@ module.exports = {
 			WHATSAPPVERSION,
 			DEVICE_NAME,
 			WA_VERSION,
-			MULTIDEVICE,
 			AUTO_CLOSE,
-			JWT_SECRET,
-			TOKENSPATCH,
-			VALIDATE_MYSQL,
-			MYSQL_HOST,
-			MYSQL_PORT,
-			MYSQL_USER,
-			MYSQL_DIALECT,
-			MYSQL_PASSWORD,
-			MYSQL_DATABASE,
-			MYSQL_TIMEZONE,
+			SECRET_KEY,
 			useHere,
 			BROWSER_WSENDPOINT,
 			START_ALL_SESSIONS
